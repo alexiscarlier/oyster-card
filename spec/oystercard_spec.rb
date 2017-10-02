0e0adb1ba9fcbef8554538934876a7523b390bd2
Alexis Carlier <alexis.carlier0@gmail.com>
2017-10-02 17:17:02 +0100

    created top up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,8 @@ describe Oystercard do
   it 'starts with a balance of 0' do
     expect(oystercard.balance).to eq 0
   end
+
+  it 'allows an oyster card to be topped up by an amount' do
+    expect{ oystercard.top_up(1) }.to change { oystercard.balance }.by 1
+  end
 end
